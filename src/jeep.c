3f4d6f34603dc6d0fa3e1cd5847e40e0abfc67aa
Halis Duraki <duraki.halis@nsoft.ba>
2017-08-03 23:49:43 +0200

    [added] main getopts

--- src/jeep.c
+++ src/jeep.c
@@ -1,17 +1,87 @@
 #include <string.h>
 
 #include "builtin.h"
+#include "jeep.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <getopt.h>
 
-static const char * const jeep_usage[] = {
-    N_("jeep [cmd] [device] | [--filename] | --verbose",
-    N_("jeep [cmd] [device] |  --verbose"),
-    NULL
+int print_usage()
+{
+    const char usage [] = {
+        "jeep \t command [-d]evice -info\n"
+        "jeep \t command [-l]ist -custom]\n"
+        "jeep \t--about --version\n"
+    };
+
+    fprintf(stderr, "%s", usage);
+
+    return -1;
 }
 
-
-int main(int argc, char **argv) 
+int 
+main(int argc, char *argv[]) 
 {
+    char *pname = argv[0];  /* parent proc name */
+    char *cmd;              /* command to exec */
+
+    char *device;           /* device uuid or mounpoint */
+	bool about = false;     /* is in about */
+    bool list = false;      /* is in list */
+
+    int getopt_ret, option_index;
+    static struct option long_options[] = {
+               {"device",  required_argument, 0, 'd'},
+               {"list",  optional_argument, 0, 'l'},
+               {0, 0, 0, 0}       };
+
+    get_art();
+	print_usage();
+
+    if (argv[1]) {
+        char *cmd = argv[1];
+
+        if (strcmp(cmd, "--about") == 0) {
+            printf("jeep - penetration testing framework for vehicle systems\n"); 
+            exit(0);
+        }
+
+        if (strcmp(cmd, "--version") == 0) {
+            exit(get_version());
+        }
+
+        init_builtin(); /* Initialize builtin cmds */    
+        printf("[*] Getting command module: %s\n", cmd);
+
+    }
+
+    while (1) {
+        getopt_ret = getopt_long( argc, argv, "d:l::",
+                                  long_options,  &option_index);
+        if (getopt_ret == -1) break;
+
+        switch(getopt_ret)
+        {
+			printf("\n");
+            case 0: break;
+            case 'd':
+                printf("Device set to: %s\n", optarg); break;
+            case 'l':
+                printf("Listing all commands: ");
+                if (optarg)
+                    printf (", %s!\n", optarg);
+                else
+                    printf ("!\n", optarg);
+                break;
+            case '?':
+                printf("\nProcess got unknown option.\n"); break;
+        }
+    } 
+    return 0;
+
+    
+    print_usage();
 
     return 1;
 }
