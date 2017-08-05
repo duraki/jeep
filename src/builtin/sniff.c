874e0baaf667b0d93c8b2943fe877b751531e85c
Halis Duraki <duraki.halis@nsoft.ba>
2017-08-05 17:11:17 +0200

    [added] init screen for sniff module

--- src/builtin/sniff.c
+++ src/builtin/sniff.c
@@ -21,6 +21,7 @@
 #include <ctype.h>
 #include <libgen.h>
 #include <time.h>
+#include <ncurses.h>
 
 #include <sys/time.h>
 #include <sys/types.h>
@@ -33,20 +34,64 @@
 #include <linux/can.h>
 #include <linux/can/bcm.h>
 
+#define MODNAME "sniff"
+#define MODVERS "1.0.0"
+
+#define INTERFACE "*"
+
 static const char * const jeep_sniff_usage[] = {
-    N_("[jeep] sniff [device]",
-    N_("[jeep] sniff [device]"),
+    "[jeep] sniff [device]",
+    "[jeep] sniff [device]",
     NULL
-}
-
-enum sniff_type { BASIC, MIXED };
-
-static const char *sniff_type_names[] = {
-    N_("basic"), N_("mixed"), NULL
 };
 
-int main(int argc, char *argv[])
-{
+enum sniff_type { BASIC, MIXED };
 
+static 
+const char *sniff_type_names[] = {
+    "basic", "mixed", NULL
+};
+
+int
+init_screen()
+{
+    initscr();
+    int x, y; /* visible area */
+    getmaxyx(stdscr, y, x);
+
+    start_color();
+    init_pair(1, COLOR_BLACK, COLOR_WHITE);
+
+    /* print modname */
+    attron(A_BOLD);
+    char modname[50];
+    sprintf(modname, "modname: [%s]", MODNAME);
+    printw(modname);
+
+    /* print modversion */
+    char modversion[50];
+    sprintf(modversion, "modversion: [%s]", MODVERS);
+    mvprintw(0, (x-strlen(modname))/2, modversion);
+
+    /* print interface */
+    char inf[50];
+    sprintf(inf, "interface: [%s]", INTERFACE);
+    mvprintw(0, x-strlen(inf), inf);
+
+    attroff(A_BOLD);
+    mvprintw(y-1, 0, "mode: %s", sniff_type_names[0]);
+
+    refresh();
+    noecho();
+    getch();
+    endwin();
+
+    return 1;
+}
+
+int 
+main(int argc, char *argv[])
+{
+    init_screen();
 }
 
