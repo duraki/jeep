c1f4158faf9ee13bed474c05fbb8302e80e99503
Halis Duraki <duraki.halis@nsoft.ba>
2017-08-05 21:29:23 +0200

    [added] list devices thr api

--- src/jeep.c
+++ src/jeep.c
@@ -1,24 +1,80 @@
+/**
+ *
+ * This is `brain` of the jeep system. The initial command parsing and api lay
+ * here. One may read this code for a better understanding when writing a
+ * module. Providing builtin system to search in/ext sub-cmds, this also
+ * transfer and collect a log.
+ *
+ * jeep.c 
+ *
+ * Copyright (c) 2017 Halis Duraki. All rights reserved.
+ *
+ * Author:
+ * Halis Duraki <duraki.halis@nsoft.com> 
+ *
+ **/
+
 #include <string.h>
 
 #include "builtin.h"
 #include "jeep.h"
+#include "builtin/api.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <getopt.h>
 #include <string.h>
+#include <ncurses.h>
 
 int print_usage()
 {
     const char usage [] = {
-        "jeep \t command [--d=xxxxxx] | device | -info\n"
-        "jeep \t command [--l=custom] | list\n"
+        "jeep \t --devices [-info]\n"
         "jeep \t --about --version\n\n"
+        "jeep \t command [--d=xxxxxx] | device | -info\n"
+        "jeep \t command [--l=custom] | list\n\n"
     };
 
     fprintf(stderr, "%s", usage);
 
     return -1;
+}
+
+int
+win_devices(struct device_i *dev_l)
+{
+    initscr();
+    int x, y; /* visible area */
+    getmaxyx(stdscr, y, x);
+    attron(A_BOLD); 
+
+    printw("%-10s %-30s %-10s %-20s %-10s %-10s\n\n", 
+            "[#]", 
+            "DEVICE",
+            "INBYTES", 
+            "INPACKETS",
+            "OUTBYTES",
+            "OUTPACKETS");
+
+    mvprintw(y-1, 0, "%-10s\n", "jeep | [device list]");
+
+    attroff(A_BOLD);
+    for (int i = 0; i < 30; i++) {
+        y = i+1;
+        if (ldevice[i].id != 0) {
+            mvprintw(y, 0, "%-10d %-30s %-10d %-20d %-10d %-10d\n", 
+                    ldevice[i].id, 
+                    ldevice[i].ifr.ifname,
+                    ldevice[i].ifr.r_bytes,
+                    ldevice[i].ifr.r_packets,
+                    ldevice[i].ifr.t_bytes,
+                    ldevice[i].ifr.t_packets);
+        }
+    }
+
+    refresh();
+    getch();
+    endwin();
 }
 
 int 
@@ -35,7 +91,7 @@ main(int argc, char *argv[])
     static struct option long_options[] = {
                {"device",  required_argument, 0, 'd'},
                {"list",  optional_argument, 0, 'l'},
-               {0, 0, 0, 0}       };
+               {0, 0, 0, 0}};
 
     get_art();
     init_builtin();         /* init builtin cmds */
@@ -44,8 +100,14 @@ main(int argc, char *argv[])
     if (argv[1]) {
         char *cmd = argv[1];
 
+        if (strcmp(cmd, "--devices") == 0) { /* todo: -info */
+            printf("[*] Searching for connected devices ...\n"); 
+            struct device_i *dev_l = get_interface_list();
+            win_devices(dev_l);
+        }
+
         if (strcmp(cmd, "--about") == 0) {
-            printf("jeep\t - penetration testing framework for vehicle systems\n"); 
+            printf("[o_o] `jeep`\t - penetration testing framework for vehicle systems\n"); 
             exit(0);
         }
 
