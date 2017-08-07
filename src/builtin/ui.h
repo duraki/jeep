c40a12c26bbf96d82c20aadcc44f5be41141dd28
Halis Duraki <duraki.halis@nsoft.ba>
2017-08-07 23:23:30 +0200

    [added] ui class / sepeare modui

---
+++ src/builtin/ui.h
@@ -0,0 +1,35 @@
+/*
+ * Display version name and module plus interface attached to it.
+ *
+ * name: Module name
+ * version: Module version
+ * iface: Interface
+ * row, col: /
+ */
+int ui_module(char *name, char *version, char *iface,
+        int row, int col)
+{
+    initscr();
+    start_color();
+    init_pair(1, COLOR_BLUE, COLOR_BLACK);
+
+    attron(COLOR_PAIR(1) | A_BOLD); /* set style */
+
+    /* print modname */
+    char modname[50];
+    sprintf(modname, "modname: [%s]", name);
+    printw(modname);
+
+    /* print modversion */
+    char modversion[50];
+    sprintf(modversion, "modversion: [%s]", version);
+    mvprintw(0, (row-strlen(modname))/2, modversion);
+
+    /* print interface */
+    char inf[50];
+    sprintf(inf, "interface: [%s]", iface);
+    mvprintw(0, row-strlen(inf), inf);
+
+    refresh();
+}
+
